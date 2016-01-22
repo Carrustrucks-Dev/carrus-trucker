547eecae1c9d4886a58e7c661543c84b5b688864
Saurabh Verma <saurabh.verma@clicklabs.in>
2016-01-22 11:31:59 +0530

    release build

--- app/src/main/java/com/carrus/trucker/utils/Config.java
+++ app/src/main/java/com/carrus/trucker/utils/Config.java
@@ -7,7 +7,7 @@ public class Config {
 
     static String GCM_PROJECT_NUMBER = "";
     static String BASE_URL = "";
-    static AppMode appMode = AppMode.LIVE;
+    static AppMode appMode = AppMode.DEV;
     static String GOOGLE_URL = "";
 
     static public String getBaseURL() {
@@ -39,19 +39,22 @@ public class Config {
         switch (appMode) {
             case DEV:
                 BASE_URL = "http://52.25.204.93:3006/";
-                GCM_PROJECT_NUMBER = "1082788264801";
+                GCM_PROJECT_NUMBER = "165982884401";   //Released
+//                GCM_PROJECT_NUMBER = "1082788264801";
                 GOOGLE_URL = "http://maps.googleapis.com";
                 break;
 
             case TEST:
                 BASE_URL = "http://52.25.204.93:3005/";
-                GCM_PROJECT_NUMBER = "1082788264801";
+                GCM_PROJECT_NUMBER = "165982884401";
+//                GCM_PROJECT_NUMBER = "1082788264801";
                 GOOGLE_URL = "http://maps.googleapis.com";
                 break;
 
             case LIVE:
                 BASE_URL = "http://52.25.204.93:8080/";
-                GCM_PROJECT_NUMBER = "1082788264801";
+                GCM_PROJECT_NUMBER = "165982884401";
+//                GCM_PROJECT_NUMBER = "1082788264801";
                 GOOGLE_URL = "http://maps.googleapis.com";
                 break;
         }
